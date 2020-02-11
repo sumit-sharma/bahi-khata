5ac000efad766e8de77241bbd136511cb6511a15
sumit-sharma <sumit.sharma@appinventiv.com>
2020-02-11 16:23:05 +0530

    register view

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-    res.render('Auth/login');``
+    res.render('Auth/login', {title:"Login Bahikhata"});
 })
 
 module.exports = router;
